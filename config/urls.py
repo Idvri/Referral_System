2b3f07baa6a7c5a89faf3ce5757d24226fa8e71e
Nodar <nodar.panchenko@gmail.com>
2023-11-24 17:46:38 +0400

    lil fix

--- config/urls.py
+++ config/urls.py
@@ -40,6 +40,6 @@ urlpatterns = [
     path('service/', include('service.urls', namespace='service')),
     path('users/', include('users.urls', namespace='users')),
 
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
